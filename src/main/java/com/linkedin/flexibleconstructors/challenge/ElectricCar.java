93451d15efa0901f8849326d80cf06847fd50361
pjs2005 <pjs2005@users.noreply.github.com>
2026-08-10 16:51:31 +0100

    my changes

--- src/main/java/com/linkedin/flexibleconstructors/challenge/ElectricCar.java
+++ src/main/java/com/linkedin/flexibleconstructors/challenge/ElectricCar.java
@@ -7,8 +7,32 @@ public class ElectricCar extends Vehicle {
     public ElectricCar(String licensePlate, int year, int batteryCapacity) {
         // TODO: Add your prologue here
         //   1. Validate year
+        if(year < 2010){
+            throw new IllegalArgumentException("Year must be 2010 or later.");
+        } 
+
         //   2. Validate batteryCapacity
         //   3. Initialize this.batteryCapacity
+
+        if(batteryCapacity >= 200){
+            throw new IllegalArgumentException("Battery capacity must be below 200 kWh.");
+        } 
+
+        if(batteryCapacity <= 40){
+            throw new IllegalArgumentException("Battery capacity must be above 40 kWh.");
+        } else {
+            this.batteryCapacity = batteryCapacity;
+            if(batteryCapacity >= 75){
+                this.chargingType = "FAST";
+            } else {
+                this.chargingType = "STANDARD";
+            }
+        }
+
+
+
+
+        
         //   4. Initialize this.chargingType
 
         // TODO: Call super constructor
@@ -16,5 +40,7 @@ public class ElectricCar extends Vehicle {
 
         // TODO: Add your epilogue here
         //   - Print configuration message
+        System.out.println("ElectricCar configuration: " + licensePlate + ", " + year + ", " + batteryCapacity + " kWh, " + chargingType);  
+
     }
 }
